46bf14ff6a94211c34b8b43cea1bc7d5860f4e08
Leandro Pereira <leandro@leandro.io>
2025-05-02 12:26:03 -0400

    chore: update queries

--- queries/python/highlights.scm
+++ queries/python/highlights.scm
@@ -35,18 +35,6 @@
     function: (identifier) @_func))
   (#any-of? @_func "TypeVar" "NewType"))
 
-; Builtin functions
-((call
-  function: (identifier) @function.builtin)
-  (#any-of? @function.builtin
-    "abs" "all" "any" "ascii" "bin" "bool" "breakpoint" "bytearray" "bytes" "callable" "chr"
-    "classmethod" "compile" "complex" "delattr" "dict" "dir" "divmod" "enumerate" "eval" "exec"
-    "filter" "float" "format" "frozenset" "getattr" "globals" "hasattr" "hash" "help" "hex" "id"
-    "input" "int" "isinstance" "issubclass" "iter" "len" "list" "locals" "map" "max" "memoryview"
-    "min" "next" "object" "oct" "open" "ord" "pow" "print" "property" "range" "repr" "reversed"
-    "round" "set" "setattr" "slice" "sorted" "staticmethod" "str" "sum" "super" "tuple" "type"
-    "vars" "zip" "__import__"))
-
 ; Function definitions
 (function_definition
   name: (identifier) @function)
@@ -92,27 +80,9 @@
 ] @string.escape
 
 ; doc-strings
-(module
-  .
-  (comment)*
-  .
-  (expression_statement
-    (string
-      (string_content) @spell) @string.documentation))
-
-(class_definition
-  body: (block
-    .
-    (expression_statement
-      (string
-        (string_content) @spell) @string.documentation)))
-
-(function_definition
-  body: (block
-    .
-    (expression_statement
-      (string
-        (string_content) @spell) @string.documentation)))
+(expression_statement
+  (string
+    (string_content) @spell) @string.documentation)
 
 ; Tokens
 [
@@ -424,6 +394,18 @@
   function: (attribute
     attribute: (identifier) @constructor))
   (#lua-match? @constructor "^%u"))
+
+; Builtin functions
+((call
+  function: (identifier) @function.builtin)
+  (#any-of? @function.builtin
+    "abs" "all" "any" "ascii" "bin" "bool" "breakpoint" "bytearray" "bytes" "callable" "chr"
+    "classmethod" "compile" "complex" "delattr" "dict" "dir" "divmod" "enumerate" "eval" "exec"
+    "filter" "float" "format" "frozenset" "getattr" "globals" "hasattr" "hash" "help" "hex" "id"
+    "input" "int" "isinstance" "issubclass" "iter" "len" "list" "locals" "map" "max" "memoryview"
+    "min" "next" "object" "oct" "open" "ord" "pow" "print" "property" "range" "repr" "reversed"
+    "round" "set" "setattr" "slice" "sorted" "staticmethod" "str" "sum" "super" "tuple" "type"
+    "vars" "zip" "__import__"))
 
 ; Regex from the `re` module
 (call
