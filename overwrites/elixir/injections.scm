3507fd43885f33e62b80b52cd46ecd4562deaada
Leandro Pereira <leandro@leandro.io>
2025-08-06 18:24:50 -0400

    feat(queries): inject MD and SQL into Elixir queries

--- overwrites/elixir/injections.scm
+++ overwrites/elixir/injections.scm
@@ -4,3 +4,19 @@
   (quoted_content) @injection.content
  (#eq? @_sigil_name "V")
  (#set! injection.language "svelte"))
+
+; MDEx
+(sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @injection.content
+ (#eq? @_sigil_name "MD")
+ (#set! injection.language "markdown"))
+
+
+; SQL
+((sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @injection.content)
+ (#eq? @_sigil_name "SQL")
+ (#set! injection.language "sql")
+ (#set! injection.combined))
